bcf0fc8928d254f46540a97509fe7ff1ff7016c9
Next Step <nextstep7557@gmail.com>
2024-06-26 19:44:00 +0545

    first commit

---
+++ js/home.js
@@ -0,0 +1,15 @@
+var country = "Nepal";
+
+function move() {
+  country = "India";
+  //   console.log(country);
+}
+
+function run() {
+  country = "USA";
+}
+
+console.log(country);
+move();
+run();
+console.log(country);
